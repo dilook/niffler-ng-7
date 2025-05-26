5630bbe7624a5289cf519ece40daad94a4caaa57
dilook <udenav@yandex.ru>
2025-05-26 17:40:31 +0400

    add: draft

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/impl/GatewayApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/impl/GatewayApiClient.java
@@ -2,9 +2,14 @@ package guru.qa.niffler.service.impl;
 
 import guru.qa.niffler.api.GatewayApi;
 import guru.qa.niffler.api.core.RestClient;
+import guru.qa.niffler.model.rest.FriendJson;
 import guru.qa.niffler.model.rest.UserJson;
 import io.qameta.allure.Step;
+import retrofit2.Call;
 import retrofit2.Response;
+import retrofit2.http.DELETE;
+import retrofit2.http.Header;
+import retrofit2.http.Query;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -37,4 +42,31 @@ public class GatewayApiClient extends RestClient {
     assertEquals(200, response.code());
     return requireNonNull(response.body());
   }
+
+  @Step("Send DELETE request /api/friends to niffler-gateway")
+  public void removeFriend(String bearerToken, String targetUsername) {
+    final Response<Void> response;
+    try {
+      response = gatewayApi.removeFriend(bearerToken, targetUsername)
+              .execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+  }
+
+  @Step("Send POST request /api/invitations/accept to niffler-gateway")
+  @Nonnull
+  public UserJson acceptInvitation(String bearerToken, String username) {
+    final Response<UserJson> response;
+    try {
+      response = gatewayApi.acceptInvitation(bearerToken, new FriendJson(username))
+              .execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return requireNonNull(response.body());
+  }
+
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/rest/FriendsV2RestTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/rest/FriendsV2RestTest.java
@@ -7,6 +7,7 @@ import guru.qa.niffler.jupiter.annotation.meta.RestTest;
 import guru.qa.niffler.jupiter.extension.ApiLoginExtension;
 import guru.qa.niffler.model.pageable.RestResponsePage;
 import guru.qa.niffler.model.rest.UserJson;
+import guru.qa.niffler.service.impl.GatewayApiClient;
 import guru.qa.niffler.service.impl.GatewayV2ApiClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,7 +19,8 @@ public class FriendsV2RestTest {
   @RegisterExtension
   private static final ApiLoginExtension apiLoginExtension = ApiLoginExtension.api();
 
-  private final GatewayV2ApiClient gatewayApiClient = new GatewayV2ApiClient();
+  private final GatewayV2ApiClient gatewayApiV2Client = new GatewayV2ApiClient();
+  private final GatewayApiClient gatewayApiClient = new GatewayApiClient();
 
   @ApiLogin
   @User(friends = 1, incomeInvitations = 1)
@@ -27,7 +29,7 @@ public class FriendsV2RestTest {
     final UserJson expectedFriend = user.testData().friends().getFirst();
     final UserJson expectedInvitation = user.testData().incomeInvitations().getFirst();
 
-    final RestResponsePage<UserJson> response = gatewayApiClient.allFriends(token, 0, 2, null, null);
+    final RestResponsePage<UserJson> response = gatewayApiV2Client.allFriends(token, 0, 2, null, null);
 
     Assertions.assertEquals(2, response.getContent().size());
 
@@ -36,5 +38,42 @@ public class FriendsV2RestTest {
 
     Assertions.assertEquals(expectedFriend.id(), actualFriend.id());
     Assertions.assertEquals(expectedInvitation.id(), actualInvitation.id());
+  }
+
+  @ApiLogin
+  @User(friends = 2, incomeInvitations = 2)
+  @Test
+  void friendsAndIncomeInvitationsListShouldBeFilteredAndReturned(UserJson user, @Token String token) {
+    final UserJson expectedFriend = user.testData().friends().getFirst();
+
+    final RestResponsePage<UserJson> response = gatewayApiV2Client.allFriends(token, 0, 4, null, expectedFriend.username());
+    Assertions.assertEquals(1, response.getContent().size());
+
+    final UserJson filteredFriend = response.getContent().getFirst();
+    Assertions.assertEquals(expectedFriend.id(), filteredFriend.id());
+  }
+
+  @ApiLogin
+  @User(friends = 1)
+  @Test
+  void friendshipShouldBeRemovable(UserJson user, @Token String token) {
+    final UserJson friend = user.testData().friends().getFirst();
+
+    gatewayApiClient.removeFriend(token, friend.username());
+    final RestResponsePage<UserJson> response = gatewayApiV2Client.allFriends(token, 0, 4, null, null);
+
+    Assertions.assertEquals(0, response.getContent().size());
+  }
+  @ApiLogin
+  @User(incomeInvitations = 1)
+  @Test
+  void incomeInvitationShouldBeAcceptable(UserJson user, @Token String token) {
+    final UserJson incomeUser = user.testData().incomeInvitations().getFirst();
+
+    gatewayApiClient.acceptInvitation(token, incomeUser.username());
+    final RestResponsePage<UserJson> response = gatewayApiV2Client.allFriends(token, 0, 4, null, null);
+
+    Assertions.assertEquals(1, response.getContent().size());
+    Assertions.assertEquals(incomeUser.id(), response.getContent().getFirst().id());
   }
 }
