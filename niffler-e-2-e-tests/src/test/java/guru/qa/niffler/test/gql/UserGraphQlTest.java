6e0e58acda8110c301c4945d8134746e403efeb3
dilook <udenav@yandex.ru>
2025-06-10 16:45:23 +0400

    add: deny subqueries for friends fix: tests UserGraphQlTest by implementation

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/gql/UserGraphQlTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/gql/UserGraphQlTest.java
@@ -5,7 +5,7 @@ import com.apollographql.apollo.api.Error;
 import com.apollographql.java.client.ApolloCall;
 import com.apollographql.java.rx2.Rx2Apollo;
 import guru.qa.UserWithFriendsCategoriesQuery;
-import guru.qa.UserWithUsersDepthThreeQuery;
+import guru.qa.UserWithUsersDepthOneQuery;
 import guru.qa.niffler.jupiter.annotation.ApiLogin;
 import guru.qa.niffler.jupiter.annotation.Token;
 import guru.qa.niffler.jupiter.annotation.User;
@@ -35,14 +35,14 @@ public class UserGraphQlTest extends BaseGraphQlTest {
     @Test
     @ApiLogin
     void shouldNotFetchMoreThan2FriendsInSubQueries(@Token String bearerToken) {
-        final ApolloCall<UserWithUsersDepthThreeQuery.Data> currenciesCall = apolloClient.query(new UserWithUsersDepthThreeQuery(0, 10, null, null))
+        final ApolloCall<UserWithUsersDepthOneQuery.Data> currenciesCall = apolloClient.query(new UserWithUsersDepthOneQuery(0, 10, null, null))
                 .addHttpHeader("authorization", bearerToken);
 
-        final ApolloResponse<UserWithUsersDepthThreeQuery.Data> response = Rx2Apollo.single(currenciesCall).blockingGet();
+        final ApolloResponse<UserWithUsersDepthOneQuery.Data> response = Rx2Apollo.single(currenciesCall).blockingGet();
         final List<Error> errors = response.errors;
         Assertions.assertNotNull(errors);
         Assertions.assertEquals(1, errors.size());
-        Assertions.assertEquals("Can`t fetch over 2 friends sub-queries", errors.getFirst().getMessage());
+        Assertions.assertEquals("Can`t fetch over 1 friends sub-queries", errors.getFirst().getMessage());
         Assertions.assertEquals("BAD_REQUEST", errors.getFirst().getExtensions().get("classification"));
     }
 
