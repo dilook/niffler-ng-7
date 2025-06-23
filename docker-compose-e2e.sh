6778a19180409c8e2096134bedf253d50d681f5a
dilook <udenav@yandex.ru>
2025-06-24 00:05:30 +0400

    bump: browser version

--- docker-compose-e2e.sh
+++ docker-compose-e2e.sh
@@ -18,9 +18,27 @@ if [ ! -z "$docker_containers" ]; then
   docker rm $docker_containers
 fi
 
-if [ ! -z "$docker_images" ]; then
-  echo "### Remove images: $docker_images ###"
-  docker rmi $docker_images
+images=("postgres" "zookeeper" "kafka" "niffler-auth-docker" "niffler-currency-docker" "niffler-gateway-docker"
+ "niffler-spend-docker" "niffler-userdata-docker" "niffler-ng-client-docker" "selenoid" "selenoid-ui"
+ "allure-docker-service" "allure-docker-service-ui")
+build_needed=false
+for image in "${images[@]}"; do
+  if ! docker images --format "{{.Repository}}" | grep -q "${image}"; then
+    echo "### Image not found: '${image}'. Build needed ###"
+    build_needed=true
+  fi
+done
+
+E2E_IMAGE=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep "niffler-e-2-e-tests")
+docker rmi "$E2E_IMAGE"
+echo "### $E2E_IMAGE removed ###"
+
+if $build_needed; then
+  echo "### Build all images except $E2E_IMAGE ###"
+  bash ./gradlew clean
+  bash ./gradlew jibDockerBuild -x :niffler-e-2-e-tests:build -x :niffler-e-2-e-tests:test
+else
+  echo "### All images exist except $E2E_IMAGE. No build needed ###"
 fi
 
 echo '### Java version ###'
@@ -28,6 +46,5 @@ java --version
 bash ./gradlew clean
 bash ./gradlew jibDockerBuild -x :niffler-e-2-e-tests:test
 
-docker pull selenoid/vnc_chrome:127.0
 docker compose up -d
 docker ps -a
